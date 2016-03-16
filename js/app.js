ae1732eadd0c2d4e55bc3bd4c083c48b7313bd04
Alexandra Innova <alexandrapgutierrez@gmail.com>
2016-03-16 09:56:55 -0500

    Arreglando header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-alert("Funciona")
+$(function(){
+	/*-------------------------------
+		# Submit search form
+	-------------------------------*/
+
+	$('.Header-form')
+		.submit(function (ev){
+			ev.preventDefault();
+			var busqueda = $(this)
+				.find('input[type="text"]')
+				.val();
+			alert("Estas buscando: " + busqueda)
+		})
+
+});
