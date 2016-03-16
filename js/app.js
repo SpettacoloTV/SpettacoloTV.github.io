2bf956b86fe74d73b6b0db36320dbd037251b545
Alexandra Innova <alexandrapgutierrez@gmail.com>
2016-03-16 12:52:07 -0500

    Interracción básica con la API, buscador funcionando

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $(function(){
+
+	var $tvShowsContainer = $('.Shows');
+
+	function renderShows (shows){
+		shows.forEach(function(show){
+			var article = template
+				.replace(':name:', show.name)
+				.replace(':img:', show.image.medium)
+				.replace(':img alt:', show.name + " Portada")
+				.replace(':status:', show.status)
+
+			var $article = $(article)
+			$tvShowsContainer.append($article)
+		})
+	}
+
 	/*-------------------------------
 		# Submit search form
 	-------------------------------*/
@@ -9,7 +25,20 @@ $(function(){
 			var busqueda = $(this)
 				.find('input[type="text"]')
 				.val();
-			alert("Estas buscando: " + busqueda)
+			$tvShowsContainer.find('.Shows-show').remove()
+			var $loader = $('<div class="loader">');
+			$loader.appendTo($tvShowsContainer);			
+			$.ajax({
+				url: 'http://api.tvmaze.com/search/shows',
+				data: { q: busqueda },
+				success: function(res, textStatus, jqXHR){
+					$loader.remove();
+					var shows = res.map(function(el){
+						return el.show;
+					})
+					renderShows(shows);
+				}
+			})
 		})
 
 	/*-------------------------------
@@ -19,7 +48,7 @@ $(function(){
 	var template = '<article class="Shows-show">' +
 	'<figure class="Shows-show-cover"><img src=":img:" class="img" alt=":img alt:">' +
           '<figcaption class="Shows-show-cover-episode">' +
-            '<p>:language:</p>' +
+            '<p>:status:</p>' +
           '</figcaption>' +
           '<figcaption class="Shows-show-cover-name">' +
             '<h2>:name:</h2><i class="fa fa-heart"></i>' +
@@ -30,17 +59,8 @@ $(function(){
 	$.ajax({
 		url: 'http://api.tvmaze.com/shows',
 		success: function(shows, textStatus, jqXHR){
-			var $tvShowsContainer = $('.Shows')
-			shows.forEach(function(show){
-				var article = template
-					.replace(':name:', show.name)
-					.replace(':img:', show.image.medium)
-					.replace(':img alt:', show.name + " Portada")
-					.replace(':language:', show.language)
-				
-				$tvShowsContainer
-					.append($(article))
-			})
+			$tvShowsContainer.find('.loader').remove();
+			renderShows(shows);
 		}
 	})
 
