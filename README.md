7811c2b75a8a0e442aba11e50434094de1841440
Alexandra Innova <alexandrapgutierrez@gmail.com>
2016-03-16 11:36:53 -0500

    Llamado básico a la API

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SpettacoloTV.github.io
+# SpettacoloTV
+
+Aplicación con HTML, CSS y jQuery, donde hacemos un llamado a la API de TVMaze http://www.tvmaze.com/api
+
+http://spettacolotv.github.io/
+
+
+Platzi
+-----------
+
+Este proyecto lo realicé en base al curso de Platzi de JavaScript y JQuery, practicando los procesos de Git y Github por el curso de la misma plataforma.
--- js/app.js
+++ js/app.js
@@ -12,4 +12,36 @@ $(function(){
 			alert("Estas buscando: " + busqueda)
 		})
 
+	/*-------------------------------
+		# Llamado Ajax
+	-------------------------------*/
+
+	var template = '<article class="Shows-show">' +
+	'<figure class="Shows-show-cover"><img src=":img:" class="img" alt=":img alt:">' +
+          '<figcaption class="Shows-show-cover-episode">' +
+            '<p>:language:</p>' +
+          '</figcaption>' +
+          '<figcaption class="Shows-show-cover-name">' +
+            '<h2>:name:</h2><i class="fa fa-heart"></i>' +
+          '</figcaption>' +
+        '</figure>' +
+      '</article>';
+
+	$.ajax({
+		url: 'http://api.tvmaze.com/shows',
+		success: function(shows, textStatus, jqXHR){
+			var $tvShowsContainer = $('.Shows')
+			shows.forEach(function(show){
+				var article = template
+					.replace(':name:', show.name)
+					.replace(':img:', show.image.medium)
+					.replace(':img alt:', show.name + " Portada")
+					.replace(':language:', show.language)
+				
+				$tvShowsContainer
+					.append($(article))
+			})
+		}
+	})
+
 });
